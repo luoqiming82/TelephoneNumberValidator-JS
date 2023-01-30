311ddec31347b0263f8cd8ea4817f4e4c3ccc833
luoqiming82 <qimingluo28@gmail.com>
2023-01-31 00:01:51 +0800

    all done and pass

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function telephoneCheck(str) {
     let strToCheck = '';
     const num = '0123456789';
-    // with country code  
-    if(str.startsWith('1 ') || str.startsWith('1(')){
+    
+    if(str.startsWith('1 ') || str.startsWith('1(')){    // with country code  
+
        for(let i=0; i<str.length; i++){
         if(i<2){
             strToCheck += str[i]; // preserve first 2 ('1 ' or '1(')
@@ -33,24 +34,35 @@ function telephoneCheck(str) {
        }
 
     }else{       //  without country code
-        
 
+        for(let i=0; i<str.length; i++){
+            if(num.includes(str[i])){  // transform  num
+                strToCheck += '5';
+            }else{
+                strToCheck += str[i]; // donot transform not nun
+            }
+        }
+        
+        switch(strToCheck){
+            case '555-555-5555':
+                return true;
+                break;
+            case '(555)555-5555':
+                return true;
+                break;
+            case '(555) 555-5555':
+                return true;
+                break;
+            case '555 555 5555':
+                return true;
+                break;
+            case '5555555555':
+                return true;
+                break;
+            default:
+                return false;
+        }
     }                                      
-    
   }
   
-
-
-
-
-
-
-
-
-
-
-
-  // test
-  //telephoneCheck("555-555-5555");
-  //
   
